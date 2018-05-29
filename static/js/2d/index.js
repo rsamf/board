60c0dd293761e3790f379ddd263cb15d3e1fcf81
Sammy Franklin <sam@Sammys-MacBook-Pro.local>
2018-05-28 17:26:09 -0700

    added styling modifiers

--- static/js/2d/index.js
+++ static/js/2d/index.js
@@ -1,43 +1,76 @@
 let board = document.getElementById("board");
 let ctx = board.getContext("2d");
 let rect = board.getBoundingClientRect();
-let lastX, lastY;
-let drawingActive = false;
+let mouse = {
+  lock: false
+};
 setup();
 
+/**
+ * Styling
+ */
+function setColor(value){
+  ctx.fillStyle=`rgb(${value[0]}, ${value[1]}, ${value[2]})`;
+}
 
+/**
+ * Image Download
+ */
+function downloadImage(){
+  let link = document.createElement('a');
+  link.href = board.toDataURL();
+  link.download = 'image';
+  link.click();
+  document.removeChild(link);
+}
 
-board.onmousemove = e => {
-  if(!drawingActive) return;
-  ctx.moveTo(lastX - rect.left, lastY - rect.top);
-  ctx.lineTo(e.clientX - rect.left, e.clientY - rect.top);
-  ctx.stroke();
-  lastX = e.clientX;
-  lastY = e.clientY;
-};
-
+/**
+ * Setup
+ */
 window.onresize = e => {
-  console.log(e);
   board.width = document.body.clientWidth;
   board.height = document.body.clientHeight;
 };
 
 function setup(){
   board.width = document.body.clientWidth;
-  board.height = document.body.clientHeight;  
+  board.height = document.body.clientHeight;
+  ctx.strokeStyle = document.getElementById("color").value;
 }
 
 /**
- * Mouse Control
+ * Controls
  */
-board.onmousedown = e => {
-  lastX = e.clientX;
-  lastY = e.clientY;
-  drawingActive = true;
+document.onkeyup = e => {
+  console.log(e.which);
+  switch(e.which) {
+    case 65:
+      downloadImage();
+      break;
+    case 16:
+      mouse.lock = !mouse.lock;
+      break;
+  }
 };
-board.onmouseup = e => drawingActive = false;
-board.onmouseleave = e => drawingActive = false;
-board.onmouseout = e => drawingActive = false;
-
-
-
+board.onmousemove = e => {
+  if(!mouse.drawingActive) return;
+  ctx.beginPath();
+  ctx.moveTo(mouse.lastX - rect.left, mouse.lastY - rect.top);
+  ctx.lineTo(e.clientX - rect.left, e.clientY - rect.top);
+  ctx.stroke();
+  mouse.lastX = e.clientX;
+  mouse.lastY = e.clientY;
+};
+board.onmousedown = e => {
+  mouse.drawingActive = !mouse.drawingActive;
+  mouse.lastX = e.clientX;
+  mouse.lastY = e.clientY;
+};
+board.onmouseup = () => {
+  if(!mouse.lock) mouse.drawingActive = false;
+};
+board.onmouseleave = () => mouse.drawingActive = false;
+board.onmouseout = () => mouse.drawingActive = false;
+document.getElementById("color").onchange = e => {
+  ctx.strokeStyle = e.target.value;
+};
