2e36896edc465da8c5b02e292999fb2030208685
Sammy Franklin <sam@Sammys-MacBook-Pro.local>
2018-06-05 14:39:54 -0700

    added many front end features

--- controllers/api.py
+++ controllers/api.py
@@ -3,3 +3,67 @@ import json
 def user():
   return response.json(auth.user)
 
+@auth.requires_login()
+def boards():
+  b = db(db.board.created_by == auth.user.id).select()
+  return response.json(b or [])
+
+@auth.requires_login()
+def board():
+  if request.args(0) is not None:
+    b = db(db.board.url == request.args(0)).select()
+    return response.json(b or [])
+  return response.json([])
+
+@auth.requires_login()
+def add_board():
+  body = request.vars
+  if body.name is not None and body.type is not None:
+    db.board.insert(name=body.name, board_type=body.type, is_public=body.is_public)
+    return True
+  return False
+
+@auth.requires_login()
+def delete_board():
+  if request.args(0) is not None:
+    q = (db.board.created_by == auth.user.id) & (db.board.url == request.args(0))
+    db(q).delete()
+    return True
+  return False
+
+@auth.requires_login()
+def edit_board_instruction():
+  body = request.vars
+  if request.args(0) is not None:
+    q = db.board.url == request.args(0)
+    board = db(q).select().first()
+    if board is not None and (board.is_public or db.board.created_by == auth.user.id):
+      board.update_record(instruction=body.instruction)
+      return True
+  return False
+
+@auth.requires_login()
+def edit_board_name():
+  body = request.vars
+  if request.args(0) is not None:
+    q = (db.board.created_by == auth.user.id) & (db.board.url == request.args(0))
+    board = db(q).select().first()
+    if board is not None:
+      board.update_record(name=body.name)
+      return True
+  return False
+
+@auth.requires_login()
+def edit_board_is_public():
+  body = request.vars
+  if request.args(0) is not None:
+    q = (db.board.created_by == auth.user.id) & (db.board.url == request.args(0))
+    board = db(q).select().first()
+    if board is not None:
+      if body.is_public is not None:
+        board.update_record(is_public=body.is_public)
+      else:
+        board.update_record(is_public=not board.is_public)
+    return True
+  return False
+  
--- static/js/index.js
+++ static/js/index.js
@@ -1,22 +1,14 @@
 
-let vue = new Vue({
-  el: "#vue",
-  delimiters: ['${', '}'],
-  unsafeDelimiters: ['!{', '}'],
-  data: {
-
-  },
-  methods: {
-
-  }
-});
-
-let networking = {
+const networking = {
   get: function(path, callback, json=true){
     let xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = () => {
-        if(xhttp.readyState === XMLHttpRequest.DONE && xhttp.status === 200) {
-            callback(json ? JSON.parse(xhttp.responseText): xhttp.responseText);
+        if(xhttp.readyState === XMLHttpRequest.DONE) {
+            if(xhttp.status === 200)
+                // console.log(xhttp.responseText);
+                callback(json ? JSON.parse(xhttp.responseText): xhttp.responseText);
+            else
+                console.error(xhttp.responseText);
         }
     };
     xhttp.open("GET", path, true);
@@ -25,8 +17,11 @@ let networking = {
   post: function(path, data, callback, json=true){
     let xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = () => {
-        if(xhttp.readyState === XMLHttpRequest.DONE && xhttp.status === 200) {
-            callback(json ? JSON.parse(xhttp.responseText): xhttp.responseText);
+        if(xhttp.readyState === XMLHttpRequest.DONE) {
+            if(xhttp.status === 200)
+                callback(json ? JSON.parse(xhttp.responseText): xhttp.responseText);
+            else 
+                console.error(xhttp.responseText)
         }
     };
     xhttp.open("POST", path, true);
@@ -36,8 +31,11 @@ let networking = {
   put: function(path, data, callback, json=true){
     let xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = () => {
-        if(xhttp.readyState === XMLHttpRequest.DONE && xhttp.status === 200) {
-            callback(json ? JSON.parse(xhttp.responseText): xhttp.responseText);
+        if(xhttp.readyState === XMLHttpRequest.DONE) {
+            if(xhttp.status === 200)
+                callback(json ? JSON.parse(xhttp.responseText): xhttp.responseText);
+            else
+                console.error(xhttp.responseText);
         }
     };
     xhttp.open("PUT", path, true);
@@ -45,3 +43,65 @@ let networking = {
     xhttp.send(JSON.stringify(data));
   }
 };
+
+let vue = new Vue({
+  el: "#vue",
+  delimiters: ['${', '}'],
+  unsafeDelimiters: ['!{', '}'],
+  data: {
+    loading: true,
+    adding: false,
+    user: null,
+    boards: [],
+    newBoard: {
+        name: "New Project",
+        type: "2D",
+        is_public: "PRIVATE"
+    }
+  },
+  methods: {
+    beginAdding: function() {
+        this.adding = true;
+    },
+    cancelAdding: function() {
+        this.adding = false;
+    },
+    submitAdding: function(){
+        let data = this.newBoard;
+        data.is_public = data.is_public === "PUBLIC";
+        networking.post(URL.ADD_BOARD, data, (res) => {
+            console.log(res);
+            this.adding = false;
+        }, false);
+    }
+
+  }
+});
+
+function setup(){
+    let req = 0;
+    networking.get(URL.USER, user => {
+        console.log("n USER", user);
+        vue.user = user;
+        checkReq();
+        if(user) {
+            networking.get(URL.BOARDS, boards => {
+                console.log("n BOARD", boards);
+                vue.boards = boards;
+                checkReq();
+            });  
+        } else {
+            checkReq();
+        }
+    });
+
+    function checkReq(){
+        req++;
+        if(req == 2) vue.loading = false;
+    }
+    
+}
+
+(function(){
+    setup();
+})();
