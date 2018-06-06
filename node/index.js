000ceeeeebf2e8eefefdbfdfa79a7aaff06925c9
Sammy Franklin <sam@eduroam-169-233-247-196.ucsc.edu>
2018-06-06 14:59:07 -0700

    finished working prototype for tcp collaboration using nodejs

---
+++ node/index.js
@@ -0,0 +1,49 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+let sockets = {};
+
+
+
+io.on('connection', function(socket){
+  console.log('connecting', socket.id);
+  sockets[socket.id] = socket;
+
+  // Join Board Session
+  socket.on('join', (room) => {
+    socket.join(room, ()=>{
+      sockets[socket.id].room = room;
+    });
+  });
+
+  // Disconnect from Board Session
+  socket.on('disconnect', function(){
+    console.log('disconnecting', socket.id);
+    delete sockets[socket.id];
+  });
+
+  // Board Control
+  socket.on("ERASE", function(data){
+    console.log(data);
+    socket.to(sockets[socket.id].room).emit("ERASE");
+  });
+
+  socket.on("LINE", function(data, hash=""){
+    console.log(data, hash);
+    socket.to(sockets[socket.id].room).emit("LINE", data, hash);
+  });
+
+  socket.on("UNDO", function(data, hash=""){
+    console.log(data, hash);
+    socket.to(sockets[socket.id].room).emit("UNDO", data, hash);
+  });
+
+});
+
+// app.get('/', ()=>{
+//   console.log("got req");
+// });
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
