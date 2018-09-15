5d2f1cbd2b9b06fe23aecfefc7eaae2612b5691e
Sammy Franklin <sam@Sammys-MacBook-Pro.local>
2018-09-14 23:17:23 -0700

    made solo branch for node server

--- node/index.js
+++ node/index.js
@@ -34,7 +34,7 @@ io.on('connection', function(socket){
   });
 
 });
-
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+const port = 80;
+http.listen(port, function(){
+  console.log('listening on *:'+port);
 });
